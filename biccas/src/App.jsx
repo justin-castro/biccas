c6aba2951579ff6f18185c81e2a6fd8d084424f2
Justin Castro <juscas13@gmail.com>
2024-03-30 17:42:17 -0500

    create basic layout

--- biccas/src/App.jsx
+++ biccas/src/App.jsx
@@ -1,35 +1,110 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="hero">
+        <nav></nav>
+        <div className="heroCard">
+          <div className="heroText"></div>
+          <div className="heroImage"></div>
+        </div>
+        <div className="collabs">
+          <div className="collabsText"></div>
+          <div className="collabLogos"></div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="offerings">
+        <div className="offeringsText">
+          <div className="offeringsTextMain"></div>
+          <div className="ratings"></div>
+        </div>
+        <div className="offeringIcons"></div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="features">
+        <div className="featuresHeading">
+          <div className="featuresHeadingMain"></div>
+          <div className="featuresHeadingSub"></div>
+          <div className="featuresHeadingButton"></div>
+        </div>
+        <div className="featuresCards">
+          <div className="featuresCard"></div>
+          <div className="featuresCard"></div>
+          <div className="featuresCard"></div>
+        </div>
+      </div>
+      <div className="benefits">
+        <div className="benefitsText">
+          <div className="benefitsHeading"></div>
+          <div className="benefitsList"></div>
+        </div>
+        <div className="benefitsImage"></div>
+      </div>
+      <div className="pricing">
+        <div className="pricingHeading">
+          <div className="pricingHeadingMain"></div>
+          <div className="pricingHeadingSub"></div>
+          <div className="pricingHeadingToggle"></div>
+        </div>
+        <div className="pricingCards">
+          <div className="pricingCard"></div>
+          <div className="pricingCard"></div>
+          <div className="pricingCard"></div>
+        </div>
+      </div>
+      <div className="testimonials">
+        <div className="testimonialSection">
+          <div className="testimonialCard">
+            <div className="testimonialHeader"></div>
+            <div className="testimonialText"></div>
+            <div className="testimonialImage"></div>
+            <div className="testimonialQuote"></div>
+            <div className="testimonialAvatars"></div>
+          </div>
+          <div className="signup">
+            <div className="signupHeading"></div>
+            <div className="signupEmailInput"></div>
+            <div className="signupEmailMessage"></div>
+            <div className="signupButton"></div>
+            <div className="signupFreeTrial"></div>
+          </div>
+        </div>
+        <div className="footer">
+          <div className="footerSection1">
+            <div className="footerLogo"></div>
+            <div className="footerSubText"></div>
+            <div className="footerSignUp"></div>
+          </div>
+          <div className="footerSection2">
+            <div className="footerLinks">
+              <div className="footerLink"></div>
+              <div className="footerLink"></div>
+              <div className="footerLink"></div>
+              <div className="footerLink"></div>
+            </div>
+            <div className="footerLinks">
+              <div className="footerLink"></div>
+              <div className="footerLink"></div>
+              <div className="footerLink"></div>
+              <div className="footerLink"></div>
+            </div>
+            <div className="footerLinks">
+              <div className="footerLink"></div>
+              <div className="footerLink"></div>
+              <div className="footerLink"></div>
+              <div className="footerLink"></div>
+            </div>
+          </div>
+
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
